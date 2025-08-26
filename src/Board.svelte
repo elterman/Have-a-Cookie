c76c51d48f35244946eea98f3e30f6ea48889c54
Arkady Elterman <elterman@gmail.com>
2025-08-26 12:40:56 -0700

    .

--- src/Board.svelte
+++ src/Board.svelte
@@ -1,10 +1,15 @@
 <script>
     import { ss } from './state.svelte';
+    import Tile from './Tile.svelte';
 
     const tiles = $derived(ss.tileSets[ss.step - 1]);
 </script>
 
-<div class="board"> </div>
+<div class="board {ss.paused ? 'paused' : ''}">
+    {#each tiles as tile (tile.row * 100 + tile.col)}
+        <Tile {tile} />
+    {/each}
+</div>
 
 <style>
     .board {
@@ -12,5 +17,10 @@
         display: grid;
         place-self: center;
         z-index: 1;
+        filter: drop-shadow(0 4px 4px #00000080);
+    }
+
+    .paused {
+        filter: brightness(0.45);
     }
 </style>
