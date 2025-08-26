e51947720ec09e1d031bfdc13f2bc3cca5f5206f
Arkady Elterman <elterman@gmail.com>
2025-08-26 13:56:00 -0700

    .

--- src/Start Prompt.svelte
+++ src/Start Prompt.svelte
@@ -9,6 +9,11 @@
     import SizeXL from '$lib/images/Size XL.webp';
     import BlackSizeXS from '$lib/images/Size XS Black.webp';
     import SizeXS from '$lib/images/Size XS.webp';
+    import Resume from '$lib/images/Resume.webp';
+    import Start from '$lib/images/Start.webp';
+    import { onSizeSet } from './shared.svelte';
+    import { _sound } from './sound.svelte';
+    import { ss } from './state.svelte';
     import ToolButton from './Tool Button.svelte';
 
     const sizes = [
@@ -19,22 +24,48 @@
         [SizeXL, BlackSizeXL],
     ];
 
-    const onClick = () => {};
+    const onSizeSelect = (op) => {
+        _sound.play('tap');
+
+        onSizeSet(op);
+    };
+
+    const onStartOrResume = () => {
+        ss.paused = false;
+
+        if (ss.secs === 0) {
+            _sound.play('dice');
+        }
+    };
 </script>
 
 <div class="start-prompt">
-    <div class="sizes">
-        {#each sizes as sob, i (i)}
-            <ToolButton id={`size-${i + 1}`} src={sob[0]} onClick={() => onClick(i + 1)}/>
-        {/each}
-    </div>
+    {#if ss.secs === 0}
+        <div class="sizes">
+            {#each sizes as sob, i (i)}
+                {@const current = i + 1 === ss.size}
+                <div class={current ? 'ro' : ''}>
+                    <ToolButton id={`size-${i + 1}`} src={sob[current ? 0 : 1]} onClick={() => onSizeSelect(i + 1)} />
+                </div>
+            {/each}
+        </div>
+        <div class="start">
+            <ToolButton id="start" src={Start} width={100} onClick={onStartOrResume} />
+        </div>
+    {:else}
+        <div class="start">
+            <ToolButton id="start" src={Resume} width={100} onClick={onStartOrResume} />
+        </div>
+    {/if}
 </div>
 
 <style>
     .start-prompt {
         grid-area: 3/1;
         display: grid;
+        gap: 20px;
         z-index: 1;
+        place-content: center;
     }
 
     .sizes {
@@ -43,5 +74,13 @@
         gap: 20px;
         place-content: center;
         filter: drop-shadow(0 2px 3px black);
+    }
+
+    .ro {
+        pointer-events: none;
+    }
+
+    .start {
+        filter: drop-shadow(0 2px 5px black);
     }
 </style>
